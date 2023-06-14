a6b522483465c87bb90e9af2702dbfeec744e39a
Egor Gasanov <Gasanov.EV11@ya.ru>
2023-06-15 02:03:19 +0300

    Решение задачи №6

---
+++ Exercise_6/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Clear();
+Console.Write ("Введите  число ");
+string userInput1 = Console.ReadLine () ??"";
+int number1 = int.Parse (userInput1);
+
+if (number1 % 2 == 0)
+{
+    Console.Write ($"Число {number1} является четным ");
+}
+else
+{
+   Console.Write ($"Число {number1} является НЕчетным "); 
+}
