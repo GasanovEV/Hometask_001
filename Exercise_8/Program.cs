bc469a0f2c91fb02f42ff14fb9780f9001742dab
Egor Gasanov <Gasanov.EV11@ya.ru>
2023-06-15 02:33:27 +0300

    Решение задачи №8

---
+++ Exercise_8/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Clear();
+Console.Write ("Введите  число ");
+string userInput1 = Console.ReadLine () ??"";
+int number1 = int.Parse (userInput1);
+
+int count = 1;
+
+while (count<=number1)
+{
+    if(count % 2 == 0)
+    Console.Write ($"{count} ");
+    count = count + 1;
+}
