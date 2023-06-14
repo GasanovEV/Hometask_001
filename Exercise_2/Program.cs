26bcbed657c10b0661ac90f5578cb01f0819a325
Egor Gasanov <Gasanov.EV11@ya.ru>
2023-06-15 01:36:05 +0300

    Решение задачи №2

---
+++ Exercise_2/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.Clear();
+Console.Write ("Введите первое число ");
+string userInput1 = Console.ReadLine () ??"";
+int number1 = int.Parse (userInput1);
+Console.Write ("Введите второе число ");
+string userInput2 = Console.ReadLine () ??"";
+int number2 = int.Parse (userInput2);
+
+if (number1 == number2)
+{
+    Console.WriteLine ($" !!! Число {number1} равно числу {number2}!!!");
+}
+    if (number1>number2)
+    {
+        Console.WriteLine ($"!!! Максимальное число -> {number1}!!!");
+    }
+    else 
+    {
+            Console.WriteLine ($"!!! Максимальное число -> {number1}!!!");
+    }
